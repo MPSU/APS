11c97ac4316ba1b1358b69479fd2867897ca60df
alexkharl <sasha_xarlamov@mail.ru>
2024-02-07 16:51:42 +0300

    Ref(01_Adder_32):Доб-ие timeunit

--- Labs/01. Adder/tb_fulladder32.sv
+++ Labs/01. Adder/tb_fulladder32.sv
@@ -9,9 +9,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module tb_fulladder32();
-`define __debug__
-parameter TIME_OPERATION  = 100;
-parameter TEST_VALUES = 3000;
+    timeunit      1ns;
+    timeprecision 1ps;
+
+    parameter TIME_OPERATION  = 100;
+    parameter TEST_VALUES = 3000;
 
     wire [31:0] tb_a_i;
     wire [31:0] tb_b_i;
