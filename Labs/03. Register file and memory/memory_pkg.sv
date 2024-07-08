22c4193a76214c20de13bf69a4b8b41967c1166c
Andrei Solodovnikov <voultboy@yandex.ru>
2024-07-08 14:03:20 +0300

    ЛР3, 6, 7. Исправление memory_pkg
    Параметр DATA_MEM_SIZE_WORDS ошибочно назывался DATA_MEM_SIZE_BYTES.

--- Labs/03. Register file and memory/memory_pkg.sv
+++ Labs/03. Register file and memory/memory_pkg.sv
@@ -13,6 +13,6 @@ package memory_pkg;
   localparam INSTR_MEM_SIZE_BYTES = 512;
   localparam INSTR_MEM_SIZE_WORDS = INSTR_MEM_SIZE_BYTES / 4;
   localparam DATA_MEM_SIZE_BYTES  = 512;
-  localparam DATA_MEM_SIZE_BYTES  = DATA_MEM_SIZE_BYTES / 4;
+  localparam DATA_MEM_SIZE_WORDS  = DATA_MEM_SIZE_BYTES / 4;
 
 endpackage
