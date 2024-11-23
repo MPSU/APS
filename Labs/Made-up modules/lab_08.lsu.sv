f6815409e235360c0e5d22dc41f19864f42d0eaf
Andrei Solodovnikov <voultboy@yandex.ru>
2024-11-23 17:18:11 +0300

    ЛР8. Обновление имени готового модуля

--- Labs/Made-up modules/lab_08.lsu.sv
+++ Labs/Made-up modules/lab_08.lsu.sv
@@ -8,7 +8,7 @@
 See https://github.com/MPSU/APS/blob/master/LICENSE file for licensing details.
 * ------------------------------------------------------------------------------
 */
-module riscv_lsu(
+module lsu(
   input logic clk_i,
   input logic rst_i,
   input  logic        core_req_i,
