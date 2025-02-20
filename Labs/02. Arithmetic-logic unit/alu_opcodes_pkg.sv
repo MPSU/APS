d0489479a3e330419e8b8b09d9156a79d6d7b733
Andrei Solodovnikov <VoultBoy@yandex.ru>
2025-02-20 18:09:56 +0300

    ЛР№2. Добавление расшифровок аббревиатур опкодов

--- Labs/02. Arithmetic-logic unit/alu_opcodes_pkg.sv
+++ Labs/02. Arithmetic-logic unit/alu_opcodes_pkg.sv
@@ -19,20 +19,20 @@ localparam ALU_OR   = 5'b00110;
 localparam ALU_AND  = 5'b00111;
 
 // shifts
-localparam ALU_SRA  = 5'b01101;
-localparam ALU_SRL  = 5'b00101;
-localparam ALU_SLL  = 5'b00001;
+localparam ALU_SRA  = 5'b01101;  // Shift_Right_Ariphmetic
+localparam ALU_SRL  = 5'b00101;  // Shift Right Logic
+localparam ALU_SLL  = 5'b00001;  // Shift Left Logic
 
 // comparisons
-localparam ALU_LTS  = 5'b11100;
-localparam ALU_LTU  = 5'b11110;
-localparam ALU_GES  = 5'b11101;
-localparam ALU_GEU  = 5'b11111;
-localparam ALU_EQ   = 5'b11000;
-localparam ALU_NE   = 5'b11001;
+localparam ALU_LTS  = 5'b11100;  // Less Than Signed
+localparam ALU_LTU  = 5'b11110;  // Less Than Unsigned
+localparam ALU_GES  = 5'b11101;  // Great [or] Equal signed
+localparam ALU_GEU  = 5'b11111;  // Great [or] Equal unsigned
+localparam ALU_EQ   = 5'b11000;  // Equal
+localparam ALU_NE   = 5'b11001;  // Not Equal
 
 // set lower than operations
-localparam ALU_SLTS = 5'b00010;
-localparam ALU_SLTU = 5'b00011;
+localparam ALU_SLTS = 5'b00010;  // Set Less Than Signed
+localparam ALU_SLTU = 5'b00011;  // Set Less Than Unsigned
 
 endpackage
